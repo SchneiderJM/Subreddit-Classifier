b0ece34a341d3042d89d31bd9d16f9fb121c0590
Jason Schneider <jasonmschneider@outlook.com>
2020-03-02 14:54:11 -0500

    initial commit to online

--- Scraper.py
+++ Scraper.py
@@ -8,6 +8,16 @@ Created on Mon Feb 24 16:17:55 2020
 
 import pandas as pd
 import os
+import praw
 
 #Client ID, secret, user agent in that order
 credentials = list(pd.read_csv(os.getcwd()+'/credentials.csv').columns)
+
+reddit = praw.Reddit(client_id=credentials[0],
+                     client_secret=credentials[1],user_agent=credentials[2])
+
+#I want:
+#Title, text, link (if applicable), top comments, author
+
+
+r = reddit.subreddit('politics').top(limit=100,time_filter='day')
